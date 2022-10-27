ec54e61ef2c9bed23e4aa186bba35121867ee0dd
nadianuan <nadia.nuan39@gmail.com>
2022-10-27 22:39:48 +0700

    Menyelesaikan Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
     <h1>{{ $post->tittle }}</h1>
         
+    <p>By. Nadia Nur Annisa in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         
     {!! $post->body !!}
 
